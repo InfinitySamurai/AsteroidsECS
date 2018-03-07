080abaf76281d2a3cd3bf935f00a4f8b1c82c157
Russell Long <russell._long@hotmail.com>
2018-03-08 09:51:00 +1100

    Added shooting cooldown

--- core/src/net/infinitycorp/asteroidsecs/systems/ShootingSystem.java
+++ core/src/net/infinitycorp/asteroidsecs/systems/ShootingSystem.java
@@ -4,8 +4,6 @@ import com.badlogic.ashley.core.*;
 import com.badlogic.ashley.utils.ImmutableArray;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import net.infinitycorp.asteroidsecs.components.*;
 import net.infinitycorp.asteroidsecs.factories.BulletFactory;
 
@@ -15,6 +13,7 @@ public class ShootingSystem extends EntitySystem{
     private ComponentMapper<CanShootComponent> canShootMapper = ComponentMapper.getFor(CanShootComponent.class);
     private ComponentMapper<PositionComponent> positionMapper = ComponentMapper.getFor(PositionComponent.class);
     private ComponentMapper<RotationComponent> rotationMapper = ComponentMapper.getFor(RotationComponent.class);
+    private ComponentMapper<ShotCooldownComponent> cooldownMapper = ComponentMapper.getFor(ShotCooldownComponent.class);
 
     private BulletFactory bulletFactory = new BulletFactory();
 
@@ -26,22 +25,33 @@ public class ShootingSystem extends EntitySystem{
 
 
     public void addedToEngine(Engine engine) {
-        entities = engine.getEntitiesFor(Family.all(PlayerControlComponent.class, CanShootComponent.class, PositionComponent.class, RotationComponent.class).get());
+        entities = engine.getEntitiesFor(Family.all(PlayerControlComponent.class, PositionComponent.class, RotationComponent.class).get());
     }
 
     public void update(float delta){
         CanShootComponent canShoot;
         PositionComponent position;
         RotationComponent rotation;
+        ShotCooldownComponent cooldown;
 
         for (Entity e : entities){
             canShoot = canShootMapper.get(e);
             position = positionMapper.get(e);
             rotation = rotationMapper.get(e);
+            cooldown = cooldownMapper.get(e);
 
             if(canShoot != null){
                 if(Gdx.input.isKeyPressed(Input.Keys.SPACE)){
                     engine.addEntity(bulletFactory.createBullet(position.x, position.y, rotation.rotation, 300));
+                    e.remove(canShoot.getClass());
+                    e.add(new ShotCooldownComponent(0.5f));
+                }
+            }
+            else if(cooldown != null){
+                cooldown.cooldown -= delta;
+                if(cooldown.cooldown < 0){
+                    e.remove(cooldown.getClass());
+                    e.add(new CanShootComponent());
                 }
             }
         }
